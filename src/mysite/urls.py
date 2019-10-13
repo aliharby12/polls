9aa71cfa97cb56334438c7e253ec1a88db0de416
ali <ali@localhost.localdomain>
2019-10-13 08:24:02 +0200

    add the static files

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from polls.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', index, name = 'index'),
     path('polls/', include('polls.urls')),
 ]
